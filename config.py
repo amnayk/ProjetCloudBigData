0f501a7a223bd9c32f95426ec6670648317d2c95
amnayk <amnayk@gmail.com>
2020-11-23 18:20:51 +0100

    config

---
+++ config.py
@@ -0,0 +1,8 @@
+# à compléter selon l'utilisateur
+
+REGION_NAME = "eu-west-3"
+ACCESS_KEY = ""
+SECRET_KEY = ""
+EC2_KEY_PAIR = "cle"
+
+# console = https://606552388196.signin.aws.amazon.com/console
