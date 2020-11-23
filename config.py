d8100f86a6d9a6dd36597ed3edc5a670f630d165
amnayk <amnayk@gmail.com>
2020-11-23 18:14:15 +0100

    ww

--- config.py
+++
@@ -1,6 +0,0 @@
-# keys à remplacer selon l'utilisateur
-
-REGION_NAME = "eu-west-3"
-ACCESS_KEY = "AKIAY2OKW6JSH57QXCWS"
-SECRET_KEY = "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"
-EC2_KEY_PAIR = "cle_amnay"
