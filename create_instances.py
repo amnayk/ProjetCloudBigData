fcba4fb89c6ae91c4bed2d72e35c638b08b0a699
Clément Chanchevrier <clement.chanchevrier@gmail.com>
2020-11-24 09:38:15 +0100

    debut cli

--- create_instances.py
+++ create_instances.py
@@ -1,27 +1,8 @@
-from private_config import REGION_NAME, ACCESS_KEY, SECRET_KEY, EC2_KEY_PAIR
-from botocore.exceptions import ClientError
-from botocore.config import Config
-import boto3
-from os import chdir, getcwd
-chdir(getcwd())
-
-
-# 21 septembre
-
-my_config = Config(
-    region_name=REGION_NAME,
-)
-
-ec2 = boto3.resource('ec2',
-                     config=my_config,
-                     aws_access_key_id=ACCESS_KEY,
-                     aws_secret_access_key=SECRET_KEY)
-
-
-ec2.create_instances(ImageId='ami-0d3f551818b21ed81',
-                     InstanceType='t2.micro',
-                     MinCount=1,
-                     MaxCount=1,
-                     )
-
+def create_instances(resource, count=1, ImageId='ami-0d3f551818b21ed81', InstanceType='t2.micro'):
+    response = resource.create_instances(ImageId=ImageId,
+                                         InstanceType=InstanceType,
+                                         MinCount=int(count),
+                                         MaxCount=int(count),
+                                         )
+    print(response)
 # ssh -i cle.pem root@[IP]  `commande shell`
---
+++ main.py
@@ -0,0 +1,57 @@
+from create_instances import create_instances
+import sys
+from private_config import REGION_NAME, ACCESS_KEY, SECRET_KEY, EC2_KEY_PAIR
+from botocore.exceptions import ClientError
+from botocore.config import Config
+import boto3
+from os import chdir, getcwd
+chdir(getcwd())
+
+
+def main():
+
+    my_config = Config(
+        region_name=REGION_NAME,
+    )
+
+    ec2 = boto3.resource('ec2',
+                         config=my_config,
+                         aws_access_key_id=ACCESS_KEY,
+                         aws_secret_access_key=SECRET_KEY)
+
+    client = boto3.client('ec2',
+                          config=my_config,
+                          aws_access_key_id=ACCESS_KEY,
+                          aws_secret_access_key=SECRET_KEY)
+
+    if len(sys.argv) == 1:
+        print('Default')
+        return 0
+    else:
+        cmd = sys.argv[1]
+        args = sys.argv[2:]
+        print(cmd, args)
+
+    if cmd == 'ci':
+        print('Creation des instances')
+        create_instances(ec2, *args)
+        return 0
+
+    elif cmd == 'create_keys':
+        print('Creation des clés')
+        create_keys(client, *args)
+        return 0
+
+    elif cmd == 'create_security_group':
+        print('Creation du groupe de sécurité')
+        create_security_gorup(ec2, *args)
+        return 0
+
+    else:
+        print("Invalid argument : " + args[0])
+        sys.exit(0)
+        return 0
+
+
+if __name__ == "__main__":
+    main()
