a35b0dfe05ada72c740d6c3b9f7f0ae48aa7359a
samuelb20 <boury.samuel82@gmail.com>
2020-12-06 15:22:32 +0100

    Modif mineure

--- cluster_k8s_ssh.py
+++ cluster_k8s_ssh.py
@@ -5,18 +5,22 @@ def lancer_k8s_ssh(CLUSTER, USER):
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     paramiko.util.log_to_file("ssh.log")
+    # Lancement de k8s sur les master nodes
     for master in CLUSTER["Masters"]:
         ssh.connect(hostname=master["Dns_Name"], username='ubuntu', pkey=k)
         ssh.exec_command('sudo apt-get update -y')
         ssh.exec_command('sudo hostnamectl set-hostname master')
         ssh.exec_command('echo "' + master["Ip_Address"] + '   master" | sudo tee -a /etc/hosts')
-        # ssh.exec_command('echo "' + + '   slave" | sudo tee -a /etc/hosts')
+        
         
         # Pour afficher la sortie standard sur notre sortie standard de la vm cible
-        ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command('cat /etc/hosts')
-        for line in iter(ssh_stdout.readline, ""):
-            print(line)
-        for line2 in iter(ssh_stderr.readline, ""):
-            print(line2)
+        # ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command('cat /etc/hosts')
+        # for line in iter(ssh_stdout.readline, ""):
+        #     print(line)
+        # for line2 in iter(ssh_stderr.readline, ""):
+        #     print(line2)
+
+    # Lancement de k8s sur les slave nodes
+    # ssh.exec_command('echo "' + + '   slave" | sudo tee -a /etc/hosts')
         
     return 0
