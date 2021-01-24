c77f12d488b50e8670f254d3d09af77e03068d1d
amnayk <amnayk@gmail.com>
2021-01-24 14:07:46 +0100

    format

--- create_instances.py
+++ create_instances.py
@@ -1,16 +1,14 @@
-def create_instances(
-    ec2_resource, security_group, NUMBER_WORKERS, NUMBER_MASTERS, KEY_NAME
-):
+def create_instances(ec2_resource, security_group, NUMBER_WORKERS, NUMBER_MASTERS, KEY_NAME):
     master_instances = ec2_resource.create_instances(
         ImageId="ami-0d3f551818b21ed81",
         MinCount=1,
         MaxCount=NUMBER_MASTERS,
         InstanceType="t2.medium",
         KeyName=KEY_NAME,
-        NetworkInterfaces=[{"AssociatePublicIpAddress": True, "DeviceIndex": 0}],
-        TagSpecifications=[
-            {"ResourceType": "instance", "Tags": [{"Key": "Type", "Value": "Master"}]}
-        ],
+        NetworkInterfaces=[
+            {'AssociatePublicIpAddress': True, 'DeviceIndex': 0}],
+        TagSpecifications=[{'ResourceType': "instance",
+                            'Tags': [{'Key': "Type", 'Value': "Master"}]}]
     )
 
     slave_instances = ec2_resource.create_instances(
@@ -19,10 +17,10 @@ def create_instances(
         MaxCount=NUMBER_WORKERS,
         InstanceType="t2.medium",
         KeyName=KEY_NAME,
-        NetworkInterfaces=[{"AssociatePublicIpAddress": True, "DeviceIndex": 0}],
-        TagSpecifications=[
-            {"ResourceType": "instance", "Tags": [{"Key": "Type", "Value": "Slave"}]}
-        ],
+        NetworkInterfaces=[
+            {'AssociatePublicIpAddress': True, 'DeviceIndex': 0}],
+        TagSpecifications=[{'ResourceType': "instance",
+                            'Tags': [{'Key': "Type", 'Value': "Slave"}]}]
     )
 
     for instance in slave_instances + master_instances:
